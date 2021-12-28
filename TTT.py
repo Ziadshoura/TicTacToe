4da5d387899606ff1e510e91f5d238903e8edcbe
Ziad Shoura <z_shoura@hotmail.com>
2021-12-27 21:23:28 -0500

    new commit

--- TTT.py
+++ TTT.py
@@ -1,82 +1,157 @@
-Player = "X"
-
-global computerPlaying 
-computerPlaying = ""
-X = [""," "," "," ",
+player = "X"
+ai = "O"# this is the ai
+move = 1 #any number just to declare it
+winner = None
+board = [""," "," "," ",
     " "," "," ",
     " "," "," "]
+gameOver = False
+
 #Detect if anyone has won yet
-def PlayerWon():
+def playerWon():
+    global winner 
+    winner = None
     #Top Row
-    if X[1] == X[2] and X[2] == X[3] and X[3] != " ":
-        return True
+    if board[1] == board[2] and board[2] == board[3] and board[3] != " ":
+        winner = board[1]
     #Middle Row
-    elif X[4] == X[5] and X[5] == X[6] and X[6] != " ":
-        return True
+    if board[4] == board[5] and board[5] == board[6] and board[6] != " ":
+        winner = board[4]
     #Bottom Row
-    elif X[7] == X[8] and X[8] == X[9] and X[9] != " ":
-        return True
+    if board[7] == board[8] and board[8] == board[9] and board[9] != " ":
+        winner = board[7]
     #Left Column
-    elif X[1] == X[4] and X[4] == X[7] and X[7] != " ":
-        return True
+    if board[1] == board[4] and board[4] == board[7] and board[7] != " ":
+        winner = board[1]
     #Middle Column
-    elif X[2] == X[5] and X[5] == X[8] and X[8] != " ":
-        return True
+    if board[2] == board[5] and board[5] == board[8] and board[8] != " ":
+        winner = board[2]
     #Right Column
-    elif X[3] == X[6] and X[6] == X[9] and X[9] != " ":
-        return True        
+    if board[3] == board[6] and board[6] == board[9] and board[9] != " ":
+        winner = board[3]        
     #Top Right to Bottom Left Diagonal
-    elif X[3] == X[5] and X[5] == X[7] and X[7] != " ":
-        return True     
+    if board[3] == board[5] and board[5] == board[7] and board[7] != " ":
+        winner = board[3]     
     #Top Left to Bottom Right Diagonal
-    elif X[1] == X[5] and X[5] == X[9] and X[9] != " ":
-        return True
+    if board[1] == board[5] and board[5] == board[9] and board[9] != " ":
+        winner = board[1]
+    
+    openSpots = 0
+    for i in range(1,10):
+        if board[i] == " ":
+            openSpots +=1
+
+    
+
+
+    if winner == None and openSpots == 0:
+        return "tie"
+
+    else:
+        return winner
+    
+    
 
 
 #Displays the board
 def printBoard():
     for i in range(10):
             print ('\n')
-    print("",X[1],"|",X[2],"|",X[3],
-        "\n",X[4],"|",X[5],"|",X[6], 
-        "\n",X[7],"|",X[8],"|",X[9])
+    print("",board[1],"|",board[2],"|",board[3],
+        "\n",board[4],"|",board[5],"|",board[6], 
+        "\n",board[7],"|",board[8],"|",board[9])
+
+def bestMove():
+    bestscore = -1000000 
+    move = 0
+    for i in range(1,10):
+        if board[i] == " ":
+            board[i] = ai
+            score = minimax(board, 0, False)
+            board[i] = " "
+            if(score >bestscore):
+                bestscore = score
+                move = i
+    board[move] = ai
+    
+scores = {
+    "X" : 10,
+    "O" : -10,
+    "tie" : 0
+}
+
+def minimax(board, depth, ismaximizing):
+    result = playerWon()
+    if result != None:
+        return scores[result]
+
+    if (ismaximizing):
+        bestScore = -1000000
+
+        for i in range(1,10):
+            if board[i] == " ":
+                board[i] = ai
+                score = minimax(board, depth+1, False)
+
+                board[i] = " "
+                bestScore = max(score, bestScore)
+    else:
+            bestScore = 1000000
+            for i in range(1,10):
+                if board[i] == " ":
+                    board[i] = player
+                    score = minimax(board, depth +1, True)
+                    board[i] = " "
+                    bestScore = min(score, bestScore)
+
+    return bestScore
+                
 
 
+    
 #while computerPlaying != "y" or computerPlaying != "n":
 print("Would you like to play against the computer(y/n)")
 computerPlaying = input()
-print(computerPlaying)
 
 if computerPlaying == "y":
-
-    printBoard()
-    #
-     
-
-
-#if
-else:
-    while not PlayerWon():
+    while not gameOver:
         printBoard()
-        print("Its your turn", Player, "where would you like to move. ")
+
+        print("Its your turn", player, "where would you like to move. ")
         Turn = int(input())
 
         #Check if turn is good
-        if X[Turn] != " ":
+        if board[Turn] != " ":
             print("This move is already taken please choose another move")
             continue
-        X[Turn] = Player
-
-        if Player == "X":
-            Player = "O"
+        board[Turn] = player
+        print(move)
+        if playerWon() == None:
+            bestMove() # this plays what the ai's best move is
         else:
-            Player = "X"
+            break
+#X,O,tie, or None and tie x and o mean the game is over
+    
 
-
-if Player == "X":
-        Player = "O"
+#if playing without computer
 else:
-        Player = "X"   
-printBoard()
-print("Good Game,", Player, "Has Won the Game")
+    while not gameOver:
+        printBoard()
+        print("Its your turn", player, "where would you like to move. ")
+        Turn = int(input())
 
+        #Check if turn is good
+        if board[Turn] != " ":
+            print("This move is already taken please choose another move")
+            continue
+        board[Turn] = player
+
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
+        if playerWon() != None:
+            break
+  
+printBoard()
+print("Good Game,", playerWon(), "Has Won the Game")
